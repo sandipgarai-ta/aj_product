15311e826bc94d069cdc1eddda9439eebfa0341e
TA-PC-1 <TA-PC-1@Sandip-TA-PC-1>
2018-10-27 14:47:46 +0530

    CORS in product

--- src/main/java/com/all/products/controllers/ProductController.java
+++ src/main/java/com/all/products/controllers/ProductController.java
@@ -1,11 +1,10 @@
 package com.all.products.controllers;
 
-import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,6 +13,7 @@ import com.all.products.models.Product;
 import com.all.products.services.ProductService;
 
 @Controller
+@CrossOrigin(origins = "*")
 @RequestMapping(value="/products")
 public class ProductController {
 	

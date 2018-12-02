27093c40fa4967d4c37b1caf55a435f86899ce7d
Jishnu-rohon <jishnu.rohon@gmail.com>
2018-12-02 14:42:39 +0530

    test war

--- src/main/java/com/all/products/AllproductsApplication.java
+++ src/main/java/com/all/products/AllproductsApplication.java
@@ -2,10 +2,17 @@ package com.all.products;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AllproductsApplication {
+public class AllproductsApplication extends SpringBootServletInitializer {  //SpringBootServletInitializer for war only
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) { //for war
+        return application.sources(AllproductsApplication.class);
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AllproductsApplication.class, args);
 	}
